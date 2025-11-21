d756ae1ec21dbd2df7dafeaf1c58db2347d57f3b
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2025-11-21 11:57:33 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,41 @@
+  <!--/Rodapé-->
+  <footer class="bg-cor-secundaria text-white">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-3">
+          <h5 class="text-primary">Atendimento</h5>
+          <ul>
+            <li>(11) 99999999</li>
+            <li>email@gmail.com</li>
+            <li>segunda a sexta das 09h00 as 17h00</li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5 class="text-primary">Atendimento</h5>
+          <ul>
+            <li>(11) 99999999</li>
+            <li>email@gmail.com</li>
+            <li>segunda a sexta das 09h00 as 17h00</li>
+          </ul>
+        </div>
+
+        <div class="col-md-3">
+          <h5 class="text-primary">Atendimento</h5>
+          <ul>
+            <li>(11) 99999999</li>
+            <li>email@gmail.com</li>
+            <li>segunda a sexta das 09h00 as 17h00</li>
+          </ul>
+        </div> 
+        <div class="col-md-3">
+          <h5 class="text-primary">Atendimento</h5>
+          <ul>
+            <li>(11) 99999999</li>
+            <li>email@gmail.com</li>
+            <li>segunda a sexta das 09h00 as 17h00</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--/.Rodapé-->
